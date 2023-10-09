6dc532cc1a0b3e6135425e14b97406bf8ae360b7
POOJA-DHAMANEKAR <pooja.dhamanekar@ekaggata.com>
2023-10-09 19:22:01 +0530

    all new 09-10-2023 changes added

--- public/firebase/firebase.js
+++ public/firebase/firebase.js
@@ -1,18 +1,11 @@
-/* import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-analytics.js"; */
-
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-
 const firebaseConfig = {
-    apiKey: "AIzaSyDjnK2rZrg7pDTqF9w05n2vJ7qqoOhH1x8",
-    authDomain: "milkvilla-52215.firebaseapp.com",
-    projectId: "milkvilla-52215",
-    storageBucket: "milkvilla-52215.appspot.com",
-    messagingSenderId: "444031611035",
-    appId: "1:444031611035:web:c07e9ad48e624008e7163e",
-    measurementId: "G-MH9N5WKZR5"
-  };
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
 
 // Initialize Firebase
 const app = firebase.initializeApp(firebaseConfig);
